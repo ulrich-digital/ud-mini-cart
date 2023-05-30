5ce65a92b769eed5e02da4360dda2d31afe1b393
anderenbenutzer <35199129+anderenbenutzer@users.noreply.github.com>
2023-05-30 10:20:59 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -17,4 +17,62 @@ function my_acf_blocks_init() {
 } // function
 
 
+/* =============================================================== *\
+   UD Mini-Cart 
+\* =============================================================== */
+function enqueue_cart_show_ajax() {
+    // wp_register_script( 'cart-show-ajax-js', get_template_directory_uri() . '/assets/js/cart-qty-ajax.js', array( 'jquery' ), '', true );
+    wp_register_script('cart-show-ajax-js', get_template_directory_uri() . '/blocks/acf_mini-cart/block.js', array('jquery'), '', true);
+    wp_localize_script('cart-show-ajax-js', 'cart_show_ajax', array('ajax_url' => admin_url('admin-ajax.php')));
+    wp_enqueue_script('cart-show-ajax-js');
+}
+add_action('wp_enqueue_scripts', 'enqueue_cart_show_ajax');
+
+function update_item_from_cart() {
+    $cart_item_key = $_POST['cart_item_key'];
+    $quantity = $_POST['qty'];
+
+    // Get mini cart
+    ob_start();
+
+    foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item) {
+        if ($cart_item_key == $_POST['cart_item_key']) {
+            $product = $cart_item['data'];
+            WC()->cart->set_quantity($cart_item_key, $quantity, $refresh_totals = true);
+
+            /*
+			$quantity = $cart_item['quantity'];
+			$subtotal = WC()->cart->get_product_subtotal( $product, $cart_item['quantity'] );
+			$price = WC()->cart->get_product_price( $product );
+			*/
+
+            /* https://rudrastyh.com/woocommerce/get-number-of-items-in-cart.html */
+            $cart_count = sprintf(_n('%d Artikel', '%d Artikel', WC()->cart->get_cart_contents_count()), WC()->cart->get_cart_contents_count());
+            $cart_count_int = WC()->cart->get_cart_contents_count();
+            $subtotal = WC()->cart->get_product_subtotal($product, $quantity); // Wichtig: per Ajax übergebener Wert verwenden und nicht nicht $cart_item['quantity']
+            $price = $product->get_price();
+        }
+    }
+    WC()->cart->calculate_totals();
+    WC()->cart->maybe_set_cart_cookies();
+    $cart_total = WC()->cart->get_cart_total();
+
+    $response = array();
+    $response['price'] = $price;
+    $response['quantity'] = $quantity;
+    $response['subtotal'] = $subtotal;
+    $response['currency'] = $currency;
+    $response['cart_count'] = $cart_count;
+    $response['cart_count_int'] = $cart_count_int;
+    $response['cart_total'] = $cart_total;
+    echo json_encode($response);
+    wp_die();
+    // return true;
+
+}
+
+add_action('wp_ajax_update_item_from_cart', 'update_item_from_cart');
+add_action('wp_ajax_nopriv_update_item_from_cart', 'update_item_from_cart');
+
+
 ?>
